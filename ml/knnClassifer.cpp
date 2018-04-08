4e72400d27c81e14e27e71db3cd0fefe993ee350
luoyao <luoyao@baidu.com>
2018-04-08 19:27:17 +0800

    完成Knn距离求取函数

--- ml/knnClassifer.cpp
+++ ml/knnClassifer.cpp
@@ -6,3 +6,68 @@
 ************************************************/
 
 #include "knnClassifer.h"
+
+#include <glog/logging.h>
+
+//#define DEBUG
+
+KnnClassifer::KnnClassifer() {
+    distance_type = EUCLIDEAN;
+};
+
+KnnClassifer::~KnnClassifer() = default;
+
+KnnClassifer::KnnClassifer(int class_nums, DISTANCE_TYPE distance_type):
+        class_nums(class_nums), distance_type(distance_type) {
+}
+
+void KnnClassifer::fit(const Eigen::MatrixXd &X, const Eigen::MatrixXd &Y) {
+
+}
+
+void KnnClassifer::predict(const Eigen::MatrixXd &X, Eigen::MatrixXd &RET) {
+
+}
+
+double KnnClassifer::calculate_distance(const Eigen::ArrayXXd &array_1, const Eigen::ArrayXXd &array_2) {
+    double distance = 0.0;
+    switch (distance_type) {
+        case DISTANCE_TYPE::EUCLIDEAN: {
+            Eigen::ArrayXXd diff = array_1 - array_2;
+#ifdef DEBUG
+            LOG(INFO) << "向量差为: " << diff << std::endl;
+#endif
+            diff = diff.pow(2);
+#ifdef DEBUG
+            LOG(INFO) << "向量平方为: " << diff << std::endl;
+#endif
+            distance = std::sqrt(diff.sum());
+#ifdef DEBUG
+            LOG(INFO) << "向量间距离为: " << distance << std::endl;
+#endif
+            break;
+        }
+        case DISTANCE_TYPE::COSINE: {
+            Eigen::ArrayXXd dot_array = array_1 * array_2;
+            double dot = dot_array.sum();
+#ifdef DEBUG
+            LOG(INFO) << "向量点积为: " << dot << std::endl;
+#endif
+            double array_1_length = std::sqrt(array_1.pow(2).sum());
+#ifdef DEBUG
+            LOG(INFO) << "向量一模长为: " << array_1_length << std::endl;
+#endif
+            double array_2_length = std::sqrt(array_2.pow(2).sum());
+#ifdef DEBUG
+            LOG(INFO) << "向量二模长为: " << array_2_length << std::endl;
+#endif
+            distance = dot / (array_1_length * array_2_length);
+#ifdef DEBUG
+            LOG(INFO) << "向量间距离为: " << distance << std::endl;
+#endif
+            break;
+        }
+    }
+    return distance;
+}
+
--- ml/knnClassifer.h
+++ ml/knnClassifer.h
@@ -8,12 +8,36 @@
 #ifndef MACHINE_LEARNING_PACKAGE_KNNCLASSIFER_H
 #define MACHINE_LEARNING_PACKAGE_KNNCLASSIFER_H
 
+#include <MLBase.h>
+#include <glog/logging.h>
 
+#include <eigen3/Eigen/Dense>
 
-class KnnClassifer {
+class KnnClassifer: public MLBase {
+public:
+    KnnClassifer();
+    ~KnnClassifer() override;
 
+    enum DISTANCE_TYPE {
+        EUCLIDEAN,
+        COSINE
+    };
+
+    KnnClassifer(int class_nums, DISTANCE_TYPE distance_type);
+
+    void fit(const Eigen::MatrixXd& X, const Eigen::MatrixXd& Y) override;
+    void predict(const Eigen::MatrixXd& X, Eigen::MatrixXd& RET) override;
+
+    void test_distance(const Eigen::ArrayXXd &array_1, const Eigen::ArrayXXd &array_2) {
+        double distance = calculate_distance(array_1, array_2);
+        LOG(INFO) << "计算距离是: " << distance << std::endl;
+    }
+
+private:
+    const int class_nums = 0;
+    DISTANCE_TYPE distance_type;
+    double calculate_distance(const Eigen::ArrayXXd &array_1, const Eigen::ArrayXXd &array_2);
 };
-
 
 
 #endif //MACHINE_LEARNING_PACKAGE_KNNCLASSIFER_H

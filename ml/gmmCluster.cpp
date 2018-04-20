660dcfc3862702ce9d33f4c8189516e15cc5d80e
luoyao <luoyao@baidu.com>
2018-04-20 21:20:54 +0800

    开始高斯混合聚类

---
+++ ml/gmmCluster.cpp
@@ -0,0 +1,8 @@
+/************************************************
+* Copyright 2014 Baidu Inc. All Rights Reserved.
+* Author: Luo Yao (luoyao@baidu.com)
+* File: gmmCluster.cpp
+* Date: 18-4-20 下午7:41
+************************************************/
+
+#include "gmmCluster.h"
---
+++ ml/gmmCluster.h
@@ -0,0 +1,19 @@
+/************************************************
+* Copyright 2014 Baidu Inc. All Rights Reserved.
+* Author: Luo Yao (luoyao@baidu.com)
+* File: gmmCluster.h
+* Date: 18-4-20 下午7:41
+************************************************/
+
+#ifndef MACHINE_LEARNING_PACKAGE_GMMCLUSTER_H
+#define MACHINE_LEARNING_PACKAGE_GMMCLUSTER_H
+
+
+
+class gmmCluster {
+
+};
+
+
+
+#endif //MACHINE_LEARNING_PACKAGE_GMMCLUSTER_H

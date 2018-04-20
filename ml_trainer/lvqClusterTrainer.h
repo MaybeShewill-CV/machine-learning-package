f2551f8b37d25b3e8ef8edb0ced239c37bded600
luoyao <luoyao@baidu.com>
2018-04-20 19:17:09 +0800

    完成lvq聚类器，并可完成二维数据的可视化

---
+++ ml_trainer/lvqClusterTrainer.h
@@ -0,0 +1,19 @@
+/************************************************
+* Copyright 2014 Baidu Inc. All Rights Reserved.
+* Author: Luo Yao (luoyao@baidu.com)
+* File: lvqClusterTrainer.h
+* Date: 18-4-20 下午5:34
+************************************************/
+
+#ifndef MACHINE_LEARNING_PACKAGE_LVQCLUSTERTRAINER_H
+#define MACHINE_LEARNING_PACKAGE_LVQCLUSTERTRAINER_H
+
+
+
+class lvqClusterTrainer {
+
+};
+
+
+
+#endif //MACHINE_LEARNING_PACKAGE_LVQCLUSTERTRAINER_H
